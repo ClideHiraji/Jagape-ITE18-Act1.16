02e8301558b7fb72812c35219133af95d354a9eb
ClideJagape <kingclidejagape122770@gmail.com>
2025-10-17 22:45:57 +0800

    Change Vite config to use src as root directory
    Updated project structure to accommodate src directory.

--- vite.config.js
+++ vite.config.js
@@ -3,17 +3,17 @@ import { defineConfig } from 'vite'
 const isCodeSandbox = 'SANDBOX_URL' in process.env || 'CODESANDBOX_HOST' in process.env
 
 export default defineConfig({
-  root: '.',                  // index.html in project root
-  publicDir: 'static',        // optional static assets folder
+  root: 'src',                  // <-- index.html is inside src/
+  publicDir: '../static',       // optional static folder
   base: process.env.NODE_ENV === 'production'
-    ? '/Jagape-ITE18-Act1.16/'  // your GitHub repo name
+    ? '/Jagape-ITE18-Act1.16/' // GitHub repo name
     : '/',
   server: {
     host: true,
     open: !isCodeSandbox
   },
   build: {
-    outDir: 'dist',           // dist folder inside project root
+    outDir: '../dist',           // build goes to root/dist
     emptyOutDir: true,
     sourcemap: true
   }
